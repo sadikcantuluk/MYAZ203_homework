d65e16159b24c97abfa30c5741ce1261e4958acf
Sadıkcan TULUK <sadikcantuluk@gmail.com>
2022-10-23 20:32:09 +0300

    H2 homework

---
+++ H2_hw/Employee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H2_NesneLAB
+{
+    internal class Employee
+    {
+        private List<Employee> employeeList = new List<Employee>();
+
+        public Employee()
+        {
+
+        }
+
+        public Employee(int ıD, string firstNmae, string lastName, int age)
+        {
+            ID = ıD;
+            this.firstNmae = firstNmae;
+            this.lastName = lastName;
+            Age = age;
+        }
+
+        public int ID { get; set; }
+        public string firstNmae { get; set; }
+        public string lastName { get; set; }
+        public int Age { get; set; }
+
+
+
+        public void AddRange(params Employee[] employee)
+        {
+            employeeList.AddRange(employee);
+        }
+
+        public void Remove(int ID)
+        {
+            employeeList.RemoveAt(ID-1);
+            
+        }
+
+        public List<Employee> ListDon()
+        {
+            return employeeList;
+        }
+
+        public override string ToString()
+        {
+            return $"{ID,-10} {firstNmae,-10} {lastName,-10} {Age,10}";
+        }
+    }
+}
---
+++ H2_hw/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H2_NesneLAB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Employee employee1 = new Employee();
+            //employee1.ID = 1;
+            //employee1.firstNmae = "SADIK";
+            //employee1.lastName = "TULUK";
+            //employee1.Age = 20;
+
+
+            var employee6 = new Employee();
+
+            employee6.AddRange(
+                new Employee() { ID = 1, firstNmae = "MURAT", lastName = "TURAN", Age = 35 },
+                new Employee() { ID = 2, firstNmae = "YUSUF", lastName = "TULUK", Age = 65 },
+                new Employee() { ID = 3, firstNmae = "ÖZCAN", lastName = "AĞIRBAŞLI", Age = 20 },
+                new Employee() { ID = 4, firstNmae = "SEFA", lastName = "TIKAÇ", Age = 21 }
+
+            );
+
+            employee6.Remove(3);
+
+            var list = employee6.ListDon();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
